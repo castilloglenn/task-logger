83ddd25c9bd13a72286e909baab8939fc21aa8f5
castilloglenn <castilloglenn@ymail.com>
2024-08-28 17:48:33 +0800

    feat: basic db

---
+++ boot.py
@@ -0,0 +1,19 @@
+import os
+import sqlite3
+
+repo_path = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(repo_path, "worklog.db")
+
+
+def init_db():
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY, timestamp TEXT, message TEXT)"""
+    )
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    init_db()
